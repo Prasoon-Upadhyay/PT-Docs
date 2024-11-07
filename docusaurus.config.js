cdeeb824b309182304acdaf20c89565dd0d933c0
Your Name <uprasoon@gmail.com>
2024-11-08 03:01:44 +0530

    nav icon change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'PANDA Terminal',
+        title: ' ',
         logo: {
           alt: 'PANDA Logo',
           src: 'img/logo.svg',
